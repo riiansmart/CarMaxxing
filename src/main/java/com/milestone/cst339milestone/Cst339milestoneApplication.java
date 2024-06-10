5a868c337078e24c27e4987eb264612aad5b0ef9
riiansmart <rismart1221@outlook.com>
2024-06-09 17:07:31 -0700

    Test

---
+++ src/main/java/com/milestone/cst339milestone/Cst339milestoneApplication.java
@@ -0,0 +1,13 @@
+package com.milestone.cst339milestone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst339milestoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst339milestoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milestone/cst339milestone/Cst339milestoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milestone.cst339milestone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst339milestoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
