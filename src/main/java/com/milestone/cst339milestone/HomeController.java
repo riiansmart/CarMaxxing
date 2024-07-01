b2b58c4f252357528fa277a1907d4ef0a10bb9de
kaihedri <kaihedrick@icloud.com>
2024-06-30 22:53:39 -0700

    trying to overwrite my damage

--- src/main/java/com/milestone/cst339milestone/HomeController.java
+++ src/main/java/com/milestone/cst339milestone/HomeController.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
